d65eb862f486f32808bd0a948cb78755f9902c3d
dario-zan <dariozanelli95@gmail.com>
2021-01-19 16:38:14 +0100

    Personal constants separated from functions

---
+++ MyConstants.py
@@ -0,0 +1,28 @@
+id2bv = {'01': 'annual mean temperature',
+         '02': 'mean diurnal range',
+         '03': 'isothermality',
+         '04': 'temperature seasonality',
+         '05': 'max temperature of warmest month',
+         '06': 'min temperature of coldest month',
+         '07': 'annual temperature range',
+         '08': 'mean temperature of wettest quarter',
+         '09': 'mean temperature of driest quarter',
+         '10': 'mean temperature of warmest quarter',
+         '11': 'mean temperature of coldest quarter',
+         '12': 'annual precipitations',
+         '13': 'precipitations of wettest month',
+         '14': 'precipitations of driest month',
+         '15': 'precipitation seasonality',
+         '16': 'precipitations of wettest quarter',
+         '17': 'precipitations of driest quarter',
+         '18': 'precipitations of driest quarter',
+         '19': 'precipitations of driest quarter'}
+
+bv_cols = ['BIO01_Centroid','BIO02_Centroid','BIO03_Centroid','BIO04_Centroid',
+           'BIO05_Centroid','BIO06_Centroid','BIO07_Centroid','BIO08_Centroid',
+           'BIO09_Centroid','BIO10_Centroid','BIO11_Centroid','BIO12_Centroid',
+           'BIO13_Centroid','BIO14_Centroid','BIO15_Centroid','BIO16_Centroid',
+           'BIO17_Centroid','BIO18_Centroid','BIO19_Centroid']
+
+bv_ids = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10',
+          '11', '12', '13', '14', '15', '16', '17', '18', '19']
