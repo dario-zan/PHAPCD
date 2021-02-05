3fad86ab76b0f56835c303e9ebcbaa2a65b2310c
dario-zan <dariozanelli95@gmail.com>
2021-02-05 14:10:28 +0100

    Changes in some data structures

--- MyConstants.py
+++ MyConstants.py
@@ -1,23 +1,3 @@
-id2bv = {'01': 'annual mean temperature',
-         '02': 'mean diurnal range',
-         '03': 'isothermality',
-         '04': 'temperature seasonality',
-         '05': 'max temperature of warmest month',
-         '06': 'min temperature of coldest month',
-         '07': 'annual temperature range',
-         '08': 'mean temperature of wettest quarter',
-         '09': 'mean temperature of driest quarter',
-         '10': 'mean temperature of warmest quarter',
-         '11': 'mean temperature of coldest quarter',
-         '12': 'annual precipitations',
-         '13': 'precipitations of wettest month',
-         '14': 'precipitations of driest month',
-         '15': 'precipitation seasonality',
-         '16': 'precipitations of wettest quarter',
-         '17': 'precipitations of driest quarter',
-         '18': 'precipitations of driest quarter',
-         '19': 'precipitations of driest quarter'}
-
 bv_cols = ['BIO01_Centroid','BIO02_Centroid','BIO03_Centroid','BIO04_Centroid',
            'BIO05_Centroid','BIO06_Centroid','BIO07_Centroid','BIO08_Centroid',
            'BIO09_Centroid','BIO10_Centroid','BIO11_Centroid','BIO12_Centroid',
@@ -26,3 +6,20 @@ bv_cols = ['BIO01_Centroid','BIO02_Centroid','BIO03_Centroid','BIO04_Centroid',
 
 bv_ids = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10',
           '11', '12', '13', '14', '15', '16', '17', '18', '19']
+
+bv_names = ['annual mean temperature', 'mean diurnal range', 'isothermality',
+            'temperature seasonality', 'max temperature of warmest month',
+            'min temperature of coldest month', 'annual temperature range',
+            'mean temperature of wettest quarter',
+            'mean temperature of driest quarter',
+            'mean temperature of warmest quarter',
+            'mean temperature of coldest quarter', 'annual precipitations',
+            'precipitations of wettest month', 'precipitations of driest month',
+            'precipitation seasonality', 'precipitations of wettest quarter',
+            'precipitations of driest quarter',
+            'precipitations of warmest quarter',
+            'precipitations of coldest quarter']
+
+id2bv = dict(zip(bv_ids, bv_names))
+
+bv_labels = [n+' '+i for i, n in zip(bv_ids, bv_names)]
